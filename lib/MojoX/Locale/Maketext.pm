c3375a93b5c7ee3d9687430bd4abf1d5e455bd3a
Вячеслав Тихановский <vti@cpan.org>
2008-11-12 12:58:38 +0000

    Import of VTI/MojoX-Locale-Maketext-0.01 from CPAN.
    gitpan-cpan-distribution: MojoX-Locale-Maketext
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         VTI/MojoX-Locale-Maketext-0.01.tar.gz
    gitpan-cpan-author:       VTI
    gitpan-cpan-maturity:     released

---
+++ lib/MojoX/Locale/Maketext.pm
@@ -0,0 +1,131 @@
+package MojoX::Locale::Maketext;
+
+use strict;
+use warnings;
+
+use base 'Mojo::Base';
+
+require Carp;
+require Locale::Maketext;
+
+our $VERSION = '0.01';
+
+__PACKAGE__->attr('_class');
+__PACKAGE__->attr('languages', default => sub { [] });
+
+sub setup {
+    my $self = shift;
+    my %options = @_;
+
+    Carp::croak(qq/Namespace is undefined/) unless $options{namespace};
+
+    my $namespace = $options{namespace};
+    my $subclass = $options{subclass} || 'I18N';
+
+    my $class = $self->_class($namespace . '::' . $subclass);
+    eval <<"";
+        package $class;
+        use base 'Locale::Maketext';
+        1;
+
+    if ($@) {
+        Carp::croak(qq/Couldn't initialize i18n "$class", "$@"/);
+    }
+}
+
+sub language {
+    my $self = shift;
+
+    my $lang = ref $self->_handle();
+    $lang =~ s/^.*:://;
+
+    return $lang;
+}
+
+sub language_tag {
+    my $self = shift;
+
+    $self->_handle()->language_tag;
+}
+
+sub _handle {
+    my $self = shift;
+
+    my $class = $self->_class;
+
+    my @languages = (@{ $self->languages });
+    return "$class"->get_handle(@languages);
+}
+
+*loc = \&localize;
+
+sub localize {
+    my $self = shift;
+
+    my $handle = $self->_handle()->maketext(@_);
+}
+
+1;
+__END__
+
+=head1 NAME
+
+MojoX::Locale::Maketext - Locale::Maketext implementation
+
+=head1 SYNOPSIS
+
+    use MojoX::Locale::Maketext;
+
+    my $i18n =
+      MojoX::Locale::Maketext->new(namespace => 'MyApp', subclass => 'I18N');
+
+=head1 DESCRIPTION
+
+L<MojoX::Locale::Maketext> is Locale::Maketext for L<Mojo>.
+
+Standart usage:
+
+    __PACKAGE__->attr('i18n', default => sub { MojoX::Locale::Maketext->new });
+
+    sub startup {
+        my $self = shift;
+
+        $self->i18n->setup(namespace => ref $self);
+    }
+
+=head1 METHODS
+
+L<MojoX::Locale::Maketext> inherits all methods from L<Mojo::Base> and
+implements the following ones.
+
+=head2 C<setup>
+
+    Setup Locale::Maketext module.
+
+    $i18n->setup(namespace => ref $self, subclass => 'I18N');
+
+=head2 C<languages>
+
+    $i18n->languages([qw/ en en-us /]);
+
+=head2 C<language>
+
+    Current selected language.
+
+    $i18n->language;
+
+=head2 C<language_tag>
+
+    Current selected language tag.
+
+    $i18n->language_tag;
+
+=head2 C<localize>
+    
+    $i18n->localize('Hello, [_1]', 'world');
+
+=head2 C<loc>
+
+    Alias to B<localize>.
+
+=cut
---
+++ t/i18n.t
@@ -0,0 +1,40 @@
+#!perl
+
+package MyTest;
+1;
+
+package MyTest::I18N::de;
+use base 'MyTest::I18N';
+
+our %Lexicon = ( 'Hello' => 'Hallo' );
+1;
+
+package MyTest::I18N::en;
+use base 'MyTest::I18N';
+
+our %Lexicon = ( _AUTO => 1 );
+1;
+
+package main;
+
+use strict;
+use warnings;
+
+use Test::More tests => 8;
+
+use_ok( 'MojoX::Locale::Maketext' );
+
+my $i18n = MojoX::Locale::Maketext->new();
+
+$i18n->setup(namespace => 'MyTest', subclass => 'I18N');
+
+$i18n->languages( [ 'de' ] );
+is( $i18n->language, 'de' );
+is( $i18n->language_tag, 'de');
+is( $i18n->loc( 'Hello' ), 'Hallo' );
+is( $i18n->localize( 'Hello' ), 'Hallo' );
+
+$i18n->languages( [ 'en' ] );
+is( $i18n->language, 'en' );
+is( $i18n->language_tag, 'en');
+is( $i18n->loc( 'Hello' ), 'Hello' );
